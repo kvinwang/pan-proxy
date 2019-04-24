cae877ea1d3bb85e0b29e13b3beb16fdac349832
Kevin Wang <wy721@qq.com>
2019-04-24 12:36:19 +0800

    add dot.sh

---
+++ dot.sh
@@ -0,0 +1,9 @@
+home() {
+	git --work-tree=$HOME --git-dir=$HOME/.files.git $@
+}
+home init
+home remote add origin https://gitee.com/kvinwang/dotfiles.git
+home fetch
+home checkout master
+home config core.excludesfile $HOME/.home_ignore
+echo ". $HOME/.bash.d/.rc" >> $HOME/.bashrc
